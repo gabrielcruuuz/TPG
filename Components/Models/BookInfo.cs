f366abc9e263cce6f99c3d52c6ed88152caa6c73
Gabriel Oliveira <gabriel.cruz2@hotmail.com>
2025-04-04 13:22:25 -0300

    fix to handle differents types of description returns

--- Components/Models/BookInfo.cs
+++ Components/Models/BookInfo.cs
@@ -6,7 +6,31 @@ namespace LaunchQ.TakeHomeProject.Components.Models
     public class BookInfo
     {
         public string Title { get; set; }
+
         public List<int> Covers { get; set; }
-        public string Description { get; set; }
+
+        [JsonPropertyName("description")]
+        public JsonElement? DescriptionRaw { get; set; }
+
+        [JsonIgnore]
+        public string Description
+        {
+            get
+            {
+                if (DescriptionRaw == null)
+                    return null;
+
+                if (DescriptionRaw.Value.ValueKind == JsonValueKind.String)
+                    return DescriptionRaw.Value.GetString();
+
+                if (DescriptionRaw.Value.ValueKind == JsonValueKind.Object &&
+                    DescriptionRaw.Value.TryGetProperty("value", out var valueProp))
+                {
+                    return valueProp.GetString();
+                }
+
+                return null;
+            }
+        }
     }
 }
